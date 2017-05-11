5f75195c016fc438916cb199c2bae5a77937a0b4
Ryuina <ryu_ina@kaist.ac.kr>
2017-05-11 12:14:17 +0900

    first version

--- filter/recipe_filter.js
+++ filter/recipe_filter.js
@@ -15,7 +15,7 @@ $( document ).ready(function() {
             var newCell3 = newRow.insertCell(2);
             var newCell4 = newRow.insertCell(3);
             
-            newCell1.innerHTML = recipes[i]["name"];
+            newCell1.innerHTML = "<a href=\"../hifi/index_backup.html\">"+recipes[i]["name"]+"</a>";
             newCell2.innerHTML = recipes[i]["time"];
             newCell3.innerHTML = recipes[i]["difficulty"];
             newCell4.innerHTML = recipes[i]["ingredient"];
@@ -38,7 +38,7 @@ $( document ).ready(function() {
     
     function dif(){
         var difBtn = document.getElementsByClassName("difbtns");
-        var Btn = document.getElementById("bb")
+        var Btn = document.getElementById("filbtn")
         Btn.onclick = function(){
             for(var i=0;i<difBtn.length;i++)
             {
@@ -50,8 +50,104 @@ $( document ).ready(function() {
         }
     }
     
+    function tim(){
+        var Btn = document.getElementById("fader");
+        Btn.oninput = function(e){
+            var v = document.getElementById("volume");
+            v.value = e.target.value;
+            v.style.color="white";
+        }
+    }
+
+    function ing(){
+        var Btn = document.getElementById("fader2");
+        Btn.oninput = function(e){
+            var v = document.getElementById("volume2");
+            v.value = e.target.value;
+            v.style.color="white";
+        }
+    }    
+    
+    function showall(){
+        var Btn = document.getElementById("allbtn");
+        Btn.onclick = function() {
+            document.getElementById("mySidenav").style.width = "0";
+            var printTable = document.getElementById("resultTable");
+
+            var numRows = printTable.rows.length;
+
+            for(var i=0; i<numRows-1;i++){
+                if(printTable.rows.length>1){
+                    printTable.deleteRow(1);
+                }
+            }
+            
+            for(var i=0;i<recipes.length;i++){
+                var newRow = printTable.insertRow(1);
+                var newCell1 = newRow.insertCell(0);
+                var newCell2 = newRow.insertCell(1);
+                var newCell3 = newRow.insertCell(2);
+                var newCell4 = newRow.insertCell(3);
+
+                newCell1.innerHTML = "<a href=\"../hifi/index_backup.html\">"+recipes[i]["name"]+"</a>";
+                newCell2.innerHTML = recipes[i]["time"];
+                newCell3.innerHTML = recipes[i]["difficulty"];
+                newCell4.innerHTML = recipes[i]["ingredient"];
+            }
+        }
+    }
+    
+    function showfil(){
+        var Btn = document.getElementById("filbtn");
+        Btn.onclick = function(){
+            
+            document.getElementById("mySidenav").style.width = "0";
+            var printTable = document.getElementById("resultTable");
+
+            var numRows = printTable.rows.length;
+
+            for(var i=0; i<numRows-1;i++){
+                if(printTable.rows.length>1){
+                    printTable.deleteRow(1);
+                }
+            }
+            
+            var difBtn = document.getElementsByClassName("difbtns");
+            console.log(difBtn[0].checked);
+            console.log(difBtn[1].checked);
+            console.log(difBtn[2].checked);
+            var maxtim = document.getElementById("fader").value;
+            var maxing = document.getElementById("fader2").value;
+            console.log(maxing);
+            console.log(maxtim);
+            for(var i = 0 ; i<recipes.length ; i++){
+                console.log(recipes[i]["name"])
+                console.log(recipes[i]["difficulty"])
+                console.log(recipes[i]["time"])
+                console.log(recipes[i]["ingredient"])
+                if((difBtn[recipes[i]["difficulty"]].checked) && (recipes[i]["time"]<=maxtim) && (recipes[i]["ingredient"]<=maxing))
+                {
+                    var newRow = printTable.insertRow(1);
+                    var newCell1 = newRow.insertCell(0);
+                    var newCell2 = newRow.insertCell(1);
+                    var newCell3 = newRow.insertCell(2);
+                    var newCell4 = newRow.insertCell(3);
+
+                    newCell1.innerHTML = "<a href=\"../hifi/index_backup.html\">"+recipes[i]["name"]+"</a>";
+                    newCell2.innerHTML = recipes[i]["time"];
+                    newCell3.innerHTML = recipes[i]["difficulty"];
+                    newCell4.innerHTML = recipes[i]["ingredient"];                        
+                }
+            }
+        }
+    }
+    
     printrecipes();
     open();
     close();
     dif();
+    tim();
+    ing();
+    showall();
+    showfil();
 });
