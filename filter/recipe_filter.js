358281bc3a937b0e1321536d464b2467e48177c4
Ryuina <ryu_ina@kaist.ac.kr>
2017-05-11 13:02:09 +0900

    add hemp

--- filter/recipe_filter.js
+++ filter/recipe_filter.js
@@ -141,6 +141,23 @@ $( document ).ready(function() {
             }
         }
     }
+    function closeHelp() {
+		var modal = document.getElementById('myModal');
+		var btn = document.getElementById("myBtn");
+		var span = document.getElementsByClassName("close")[0];
+		btn.onclick = function() {
+			modal.style.display = "block";
+		}
+		span.onclick = function() {
+			modal.style.display = "none";
+		}
+		window.onclick = function(event) {
+			if (event.target == modal) {
+	    		modal.style.display = "none";
+			}
+		}
+
+    }
     
     printrecipes();
     open();
@@ -150,4 +167,5 @@ $( document ).ready(function() {
     ing();
     showall();
     showfil();
+    closeHelp();
 });
--- index.js
+++ index.js
@@ -56,9 +56,29 @@ $(document).ready(function() {
 	        this.value = "";
 	    }); 
 	
-    })};
+    })}
+
+   	function closeHelp() {
+		var modal = document.getElementById('myModal');
+		var btn = document.getElementById("myBtn");
+		var span = document.getElementsByClassName("close")[0];
+		btn.onclick = function() {
+			modal.style.display = "block";
+		}
+		span.onclick = function() {
+			modal.style.display = "none";
+		}
+		window.onclick = function(event) {
+			if (event.target == modal) {
+	    		modal.style.display = "none";
+			}
+		}
+
+    }
+
     focusOn();
 	enter();
 	searchClick();
+	closeHelp();
 
 });
