d64cbee4925c0d35ae7952448609734b5812083d
Ryuina <ryu_ina@kaist.ac.kr>
2017-05-22 01:17:02 +0900

    add change menu result

---
+++ menuList.js
@@ -0,0 +1,4 @@
+var menuList={
+"egg" : ["eggmal-i", "fried egg", "stim egg"],
+"bab,curry" : ["curry rice", "curry ramen", "currycurry"],
+"egg,bab" : ["egg bab", "fried rice", "egg bab good", "eggmoneyna"]}
