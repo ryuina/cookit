3d563e8f8f2b88c437aa14ad128a16c2c517f6ce
Honey618 <rlawogjs618@kaist.ac.kr>
2017-05-22 08:23:40 +0900

    js complete. with get info with html domain

--- filter/recipe.js
+++ filter/recipe.js
@@ -1,12 +1,23 @@
 var recipes = [
     //{"name" : "", "time" : "", "difficulty" : "", "ingredient" : ""},
     //difficulty -> 0 : easy 1 : normal 2 : hard
-    {"name" : "classic Aglio e olio", "time" : "15", "difficulty" : "0", "ingredient" : "2"},
-    {"name" : "Aglio e olio with Bacon", "time" : "20", "difficulty" : "1", "ingredient" : "4"},
-    {"name" : "Aglio e olio with Tomato", "time" : "15", "difficulty" : "0", "ingredient" : "3"},
-    {"name" : "Aglio e olio with seafood", "time" : "20", "difficulty" : "2", "ingredient" : "5"},
-    {"name" : "Kimchi Aglio e olio", "time" : "15", "difficulty" : "1", "ingredient" : "2"},
-    {"name" : "Spicy Aglio e olio", "time" : "10", "difficulty" : "0", "ingredient" : "4"},
-    {"name" : "Aglio e olio for kids", "time" : "5", "difficulty" : "0", "ingredient" : "2"},
-    {"name" : "Ramen Aglio e olio", "time" : "10", "difficulty" : "0", "ingredient" : "1"}
+    
+//menu : Fried Rice
+{"menu" : "FriedRice", "name" : "Fried Rice Restaurant Style", "time" : "10", "difficulty" : "1", "ingredients" : ['Rice', 'Carrot', 'Green pea', 'Egg', 'Soy sauce', 'vegetable oil', 'sesame oil'], "img" : "https://cdn.cpnscdn.com/static.coupons.com/ext/kitchme/images/recipes/600x400/fried-rice-restaurant-style_6070.jpg"},
+{"menu" : "FriedRice", "name" : "Vegetable Fried Rice", "time" : "15", "difficulty" : "0", "ingredients" : ['Rice', 'Onion', 'Carrot', 'Garlic', 'Soy sauce', 'Egg', 'scallions', 'sesame oil', 'white pepper', 'peas'], "img" : "http://www.seriouseats.com/recipes/assets_c/2016/01/20160206-fried-rice-food-lab-68-thumb-1500xauto-429632.jpg"},
+{"menu" : "FriedRice", "name" : "Chinese Fried Rice", "time" : "15", "difficulty" : "2", "ingredients" : ['Onion', 'Egg', 'Soy sauce', 'Pork', 'Carrot (very small)', 'Peas', 'Rice', 'Green onions', 'bean sprouts'], "img" : "http://www.recipetineats.com/wp-content/uploads/2016/05/Chinese-Fried-Rice_5-680x454.jpg"},
+{"menu" : "FriedRice", "name" : "Chicken Fried Rice", "time" : "20", "difficulty" : "0", "ingredients" : ['Chicken breast', 'Rice', 'Sesame oil', 'Onion', 'peas', 'Carrots', 'Soy sauce', 'Eggs', 'Green onions'], "img" : "http://www.chineserecipesforall.com/images/uploads/recipes/chicken-fried-rice-recipe-RecipeMain-Chinese-Recipes-For-All.jpg"},
+{"menu" : "FriedRice", "name" : "Egg Fried Rice", "time" : "10", "difficulty" : "0", "ingredients" : ['Egg', 'Sesame oil', 'Vegetable oil', 'Rice', 'peas' , 'Onions', 'beansprouts', 'Soy sauce', 'white pepper'], "img" : "http://ichef.bbci.co.uk/food/ic/food_16x9_448/recipes/eggfriedrice_67782_16x9.jpg"},
+
+//menu : Pasta
+{"menu" : "Pasta", "name" : "Basic Aglio e Olio", "time" : "10", "difficulty" : "0", "ingredients" : ['peperoncino', 'Garlic', 'phedelini', 'olive oil', 'salt'], "img" : "http://cdn.noshon.it/wp-content/uploads/2011-01-24-spaghetti-aglio-olio-4.jpg"},
+{"menu" : "Pasta", "name" : "Fresh Vongola Verace", "time" : "15", "difficulty" : "1", "ingredients" : []},
+{"menu" : "Pasta", "name" : "Creamy Carbonara", "time" : "15", "difficulty" : "2", "ingredients" : ['bacon', 'garlic', 'phedelini', 'Egg', 'parmesan', 'olive oil'], "img" : "http://media.foodnetwork.ca/recipetracker/dmm/C/R/Creamy_Chicken_Carbonara_001.jpg"},
+{"menu" : "Pasta", "name" : "Baek’s tomato sausage pasta", "time" : "15", "difficulty" : "1", "ingredients" : []},
+
+//menu : Sandwich
+{"menu" : "Sandwich", "name" : "Cheese-egg sandwich", "time" : "10", "difficulty" : "0", "ingredients" : []},
+{"menu" : "Sandwich", "name" : "Jam-cheese hot sandwich", "time" : "25", "difficulty" : "1", "ingredients" : []},
+{"menu" : "Sandwich", "name" : "cheese sandwich with mushroom", "time" : "10", "difficulty" : "1", "ingredients" : []},
+{"menu" : "Sandwich", "name" : "corn cheese baguette sandwich", "time" : "15", "difficulty" : "2", "ingredients" : []},
 ];
--- filter/recipe_filter.js
+++ filter/recipe_filter.js
@@ -2,170 +2,115 @@
 // has finished loading in the browser.
 
 $( document ).ready(function() {
-    //ideas=[];
     var recipe_table = recipes;
     
-    function printrecipes(){
-        var printTable = document.getElementById("resultTable");
-        
-        for(var i=0;i<recipes.length;i++){
-            var newRow = printTable.insertRow(1);
-            var newCell1 = newRow.insertCell(0);
-            var newCell2 = newRow.insertCell(1);
-            var newCell3 = newRow.insertCell(2);
-            var newCell4 = newRow.insertCell(3);
-            
-            newCell1.innerHTML = "<a href=\"../hifi/index_backup.html\">"+recipes[i]["name"]+"</a>";
-            newCell2.innerHTML = recipes[i]["time"];
-            newCell3.innerHTML = recipes[i]["difficulty"];
-            newCell4.innerHTML = recipes[i]["ingredient"];
-        }
-    }
-
-    function open() {
-        var openBtn = document.getElementById("openBtn");
-        openBtn.onclick = function() {
-            document.getElementById("mySidenav").style.width = "40%";
-        }
+    function getParameter() {
+        var parameters = location.search.substring(1).split("&");
+        //console.log(parameters);
+        menu = parameters[0].split("=")[1];
+        //console.log(menu);
+        ingredients = parameters[1].split("=")[1].split(',');
+        //console.log(ingredients);
     }
     
-    function close() {
-        var closeBtn = document.getElementById("closeBtn");
-        closeBtn.onclick = function() {
-            document.getElementById("mySidenav").style.width = "0";
+    function showTitle() {
+        var nc = menu[0];
+        for(var i=1; i<menu.length; i++)
+        {
+            if(menu[i].toUpperCase() == menu[i])
+                nc = nc + " " + menu[i];
+            else
+                nc = nc + menu[i];
         }
-    }
-    
-    function dif(){
-        var difBtn = document.getElementsByClassName("difbtns");
-        var Btn = document.getElementById("filbtn")
-        Btn.onclick = function(){
-            for(var i=0;i<difBtn.length;i++)
+        $(".menuName").html('"'+nc+'"');
+        var content='- based on ';
+        for(var i=0; i<ingredients.length; i++)    
+        {
+            if(i==ingredients.length-1)
             {
-                if(difBtn[i].checked)
+                content = content + ingredients[i] + ".";
+            }
+            else
+            {
+                content = content + ingredients[i] + ", ";     
+            }
+            
+        }
+        $(".ingredientName").html(content);
+    }
+    
+    function fillcontent(img, name, difficulty, time, needigr) {
+        var difimg;
+        if(difficulty == 0)
+            difimg = "img/Easy.png";
+        else if(difficulty == 1)
+            difimg = "img/Middle.png";
+        else
+            difimg = "img/Hard.png";
+        
+        content = '<div class="recipeBox"><div class="recipeLink"><div class="recipeImg"><img src="' + img + '"></div><div class="recipeTitle"><div class="options"><div class="option1"><img src="' + difimg + '"></div><div class="option2"><img src="img/Time.png"><span class="time">' + time + '</span></div><div class="option3"><img src="img/Ingredient_needed.png"><span class="ingredient">+' + needigr + '</span></div></div><div class="recipeName">' + name + '</div></div></div><div class="container-19"></div></div>'
+        
+        return content;
+    }
+    
+    function showinit() {
+        $("#recipes").html("")
+        var tmpcontent = "";
+        var cnt;
+        var flag;
+        var time;
+        var ingredient;
+        var difficulty;
+        
+        for(var i=0; i<recipes.length; i++)
+        {
+            flag = 0;
+            tmpcnt = "";
+            cnt = 0;
+            if(recipes[i]["menu"] == menu)
+            {
+                //console.log(recipes[i]["ingredients"]);
+                for(var j=0; j<recipes[i]["ingredients"].length; j++)
                 {
-                    console.log(i);
+                    for(var k=0; k<ingredients.length;k++)
+                    {
+                        if(ingredients[k]==recipes[i]["ingredients"][j])
+                        {
+                            console.log(recipes[i]["ingredients"][j]);
+                            flag = 1;
+                            cnt ++;
+                        }
+                    }
                 }
-            }
-        }
-    }
-    
-    function tim(){
-        var Btn = document.getElementById("fader");
-        Btn.oninput = function(e){
-            var v = document.getElementById("volume");
-            v.value = e.target.value;
-            v.style.color="white";
-        }
-    }
-
-    function ing(){
-        var Btn = document.getElementById("fader2");
-        Btn.oninput = function(e){
-            var v = document.getElementById("volume2");
-            v.value = e.target.value;
-            v.style.color="white";
-        }
-    }    
-    
-    function showall(){
-        var Btn = document.getElementById("allbtn");
-        Btn.onclick = function() {
-            document.getElementById("mySidenav").style.width = "0";
-            var printTable = document.getElementById("resultTable");
-
-            var numRows = printTable.rows.length;
-
-            for(var i=0; i<numRows-1;i++){
-                if(printTable.rows.length>1){
-                    printTable.deleteRow(1);
-                }
-            }
-            
-            for(var i=0;i<recipes.length;i++){
-                var newRow = printTable.insertRow(1);
-                var newCell1 = newRow.insertCell(0);
-                var newCell2 = newRow.insertCell(1);
-                var newCell3 = newRow.insertCell(2);
-                var newCell4 = newRow.insertCell(3);
-
-                newCell1.innerHTML = "<a href=\"../hifi/index_backup.html\">"+recipes[i]["name"]+"</a>";
-                newCell2.innerHTML = recipes[i]["time"];
-                newCell3.innerHTML = recipes[i]["difficulty"];
-                newCell4.innerHTML = recipes[i]["ingredient"];
-            }
-        }
-    }
-    
-    function showfil(){
-        var Btn = document.getElementById("filbtn");
-        Btn.onclick = function(){
-            
-            document.getElementById("mySidenav").style.width = "0";
-            var printTable = document.getElementById("resultTable");
-
-            var numRows = printTable.rows.length;
-
-            for(var i=0; i<numRows-1;i++){
-                if(printTable.rows.length>1){
-                    printTable.deleteRow(1);
-                }
-            }
-            
-            var difBtn = document.getElementsByClassName("difbtns");
-            console.log(difBtn[0].checked);
-            console.log(difBtn[1].checked);
-            console.log(difBtn[2].checked);
-            var maxtim = document.getElementById("fader").value;
-            var maxing = document.getElementById("fader2").value;
-            console.log(maxing);
-            console.log(maxtim);
-            for(var i = 0 ; i<recipes.length ; i++){
-                console.log(recipes[i]["name"])
-                console.log(recipes[i]["difficulty"])
-                console.log(recipes[i]["time"])
-                console.log(recipes[i]["ingredient"])
-                if((difBtn[recipes[i]["difficulty"]].checked) && (recipes[i]["time"]<=maxtim) && (recipes[i]["ingredient"]<=maxing))
+                if(flag)
                 {
-                    var newRow = printTable.insertRow(1);
-                    var newCell1 = newRow.insertCell(0);
-                    var newCell2 = newRow.insertCell(1);
-                    var newCell3 = newRow.insertCell(2);
-                    var newCell4 = newRow.insertCell(3);
-
-                    newCell1.innerHTML = "<a href=\"../hifi/index_backup.html\">"+recipes[i]["name"]+"</a>";
-                    newCell2.innerHTML = recipes[i]["time"];
-                    newCell3.innerHTML = recipes[i]["difficulty"];
-                    newCell4.innerHTML = recipes[i]["ingredient"];                        
+                    tmpcontent = fillcontent(recipes[i]["img"], recipes[i]["name"], recipes[i]["difficulty"], recipes[i]["time"], recipes[i]["ingredients"].length-cnt)
+                    $("#recipes").append(tmpcontent);
                 }
+                else
+                {
+                    continue;
+                }
+                    
             }
         }
     }
-    function closeHelp() {
-		var modal = document.getElementById('myModal');
-		var btn = document.getElementById("myBtn");
-		var span = document.getElementsByClassName("close")[0];
-		btn.onclick = function() {
-			modal.style.display = "block";
-		}
-		span.onclick = function() {
-			modal.style.display = "none";
-		}
-		window.onclick = function(event) {
-			if (event.target == modal) {
-	    		modal.style.display = "none";
-			}
-		}
-
+    
+    function filterEvent() {
+        $("#filterBtn").click(function(){
+            $("#filtertab").show('slow');
+            $("#greycover").show();
+            //$("#recipeList").css('opacity', '0.4');
+        });
+        $("#closeBtn").click(function(){
+            $("#filtertab").hide('slow');
+            $("#greycover").hide();
+            //$("#recipeList").css('opacity', '1');
+        });
     }
     
-    printrecipes();
-    open();
-    close();
-    dif();
-    tim();
-    ing();
-    showall();
-    showfil();
-    closeHelp();
+    filterEvent();
+    getParameter();
+    showTitle();
+    showinit();
 });
