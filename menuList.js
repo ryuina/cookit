3a66109ef5e8940881bc1e164b487f2889218cdd
ryuina <ryu_ina@kaist.ac.kr>
2017-05-31 17:44:32 +0900

    cancel data change

--- menuList.js
+++ menuList.js
@@ -1,13 +1,13 @@
 var menuList={
 "egg" : ["Eggmal-i", "Fried Egg", "Stim Egg"],
 "rice" : ["Curry Rice"],
-"egg,rice" : ["Egg Rice", "Fried Rice", "Bibimbap"],
-"rice,egg" : ["Egg Rice", "Fried Rice", "Bibimbap"],
-"egg,rice,carrot" : ["Egg Rice", "Fried Rice", "Bibimbap"],
+"egg,rice" : ["Egg Rice", "FriedRice", "Bibimbap"],
+"rice,egg" : ["Egg Rice", "FriedRice", "Bibimbap"],
+"egg,rice,carrot" : ["Egg Rice", "FriedRice", "Bibimbap"],
 "garlic,phedelini" : ["Pasta"],
 "phedelini,garlic" : ["Pasta"],
-"rice,garlic": ["Fried Rice"],
-"garlic,rice": ["Fried Rice"]}
+"rice,garlic": ["FriedRice"],
+"garlic,rice": ["FriedRice"]}
 
 var menuSrc={
 	"Eggmal-i" : "http://cfile10.uf.tistory.com/image/187CD245504C5914233C22",
@@ -15,7 +15,7 @@ var menuSrc={
 	"Stim Egg" : "http://cfile28.uf.tistory.com/image/23122748563433A1109DAA",
 	"Curry Rice" : "http://www.homecuisine.co.kr/files/attach/images/142/748/27f9c0691d8fce43983ad07e4e6e7055_2.JPG",
 	"Curry Ramen" : "http://cfile215.uf.daum.net/image/115352345064DF930E8DCA",
-	"Fried Rice" : "https://i.ytimg.com/vi/kgLI5FRfbb0/maxresdefault.jpg",
+	"FriedRice" : "https://i.ytimg.com/vi/kgLI5FRfbb0/maxresdefault.jpg",
 	"Pasta" : "http://www.foodncafe.com/swfupload/UpLoad/20140425/11/1a3533464595f5e178c86eb8aa24b4a0.jpg",
 	"Bibimbap" : "http://cfile221.uf.daum.net/image/1713073B4D435D58161122",
 	"Egg Rice" : "http://cdn-ak.f.st-hatena.com/images/fotolife/e/enjoyprojectkyoto7/20151029/20151029131606.jpg"
